647b0af52700fbcbe4758d142b799c7c239d4623
student <margarettwmboi@gmail.com>
2023-04-18 20:53:54 +0300

    flow

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+fizzBuzz()
+}
+
+//Write a function that prints each number from 1 to 100 on a new line. For each
+//multiple of 3, print "Fizz" instead of the number. For each multiple of 5, print
+//"Buzz" instead of the number. For numbers which are multiples of both 3 and
+//5, print "FizzBuzz" instead of the number.
+fun fizzBuzz() {
+    for (num in 1..100){
+        if (num % 3 == 0 && num % 5 ==0)
+            println("FizzBuzz")
+        else if (num % 5 == 0){
+            println("Buzz")
+        }
+        else if(num % 3 == 0 ){
+            println("Fizz")
+        }
+        else{
+            println(num)
+        }
+    }
+}
